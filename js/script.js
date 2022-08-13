ae373a3e46695448f749a7aa61f2c4347df4a99f
dlsduddl <inyoung106@naver.com>
2022-08-13 16:45:31 +0900

    220813 메가메뉴바 수정

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
-// $(document).ready(function(){
-//   $("header nav ul.gnb > li").hover(function(){
-//     $("ul.sub").stop().slideDown();
-//   },function(){
-//     $("ul.sub").stop().slideUp();
-//   });
- 
- 
-//  }); //ready end
-
 console.clear();
 
-function SliderBox1__init() {
-  let SliderBox1__i = 0;
-
-  setInterval(function() {
-    $('.slider-box-1').attr('data-index', ++SliderBox1__i % 3);
-  }, 3000);
-}
-
-SliderBox1__init();
+$(".menu-bar > ul").mouseenter(function(){
+  $(".nav-box").addClass("active");
+});
+$(".menu-bar").mouseleave(function(){
+  $(".nav-box").removeClass("active");
+});
